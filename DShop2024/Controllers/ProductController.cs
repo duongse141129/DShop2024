8947f310c3bcb56589a370e96cb78999d1feef3e
duongse141129 <duong97498@gmail.com>
2024-12-12 17:07:13 +0700

    Show productDetails

--- DShop2024/Controllers/ProductController.cs
+++ DShop2024/Controllers/ProductController.cs
@@ -1,17 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DShop2024.Controllers
 {
 	public class ProductController : Controller
 	{
+		private readonly DShopContext _dataContext;
+
+		public ProductController(DShopContext context)
+		{
+			_dataContext = context;
+		}
 		public IActionResult Index()
 		{
 			return View();
 		}
 
-		public IActionResult Details()
+		public async Task<IActionResult> Details(int Id)
 		{
-			return View();
+			if(Id == null) return RedirectToAction("Index");
+			var product = await _dataContext.Products
+										.Where(p => p.Id == Id)
+										.Where(p => p.Status == 1)
+										.Include(p => p.Brand)
+										.Include(p => p.Category)
+										.FirstOrDefaultAsync();
+			
+			return View(product);
 		}
 	}
 }
