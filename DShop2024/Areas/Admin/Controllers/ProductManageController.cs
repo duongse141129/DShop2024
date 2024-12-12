35f3ae3bb82809b969a67843e427d5134c1edc15
duongse141129 <duong97498@gmail.com>
2024-12-12 23:58:27 +0700

    Create Areas Admin, Add layout

---
+++ DShop2024/Areas/Admin/Controllers/ProductManageController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DShop2024.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class ProductManageController : Controller
+	{
+		private readonly DShopContext _dataContext;
+
+		public ProductManageController(DShopContext context)
+		{
+			_dataContext = context;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
--- DShop2024/Program.cs
+++ DShop2024/Program.cs
@@ -46,6 +46,11 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapControllerRoute(
+	name: "Areas",
+	pattern: "{area:exists}/{controller=ProductManage}/{action=Index}/{id?}");
+
+
 //Seed data
 //var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<DShopContext>();
 //SeedData.SeedingData(context);
