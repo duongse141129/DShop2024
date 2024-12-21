b4a786e853f79e104f95a350d3ba4b66b5b856b3
duongse141129 <duong97498@gmail.com>
2024-12-21 23:42:02 +0700

    AddToCart,UpdateStatusOrder with ajax

--- DShop2024/Areas/Admin/Controllers/OrderController.cs
+++ DShop2024/Areas/Admin/Controllers/OrderController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
 using Microsoft.EntityFrameworkCore;
 
 namespace DShop2024.Areas.Admin.Controllers
@@ -25,16 +26,61 @@ namespace DShop2024.Areas.Admin.Controllers
         public async Task<IActionResult> ViewOrder(int Id )
         {
             {
-                var order = await _context.Orders.Include(o => o.User).Include(od => od.OrderDetails).ThenInclude(p => p.Product).FirstOrDefaultAsync(o => o.Id == Id);
-                //order.OrderDetails = await _context.OrderDetails.Include(o => o.Product)
-                //                                                .Where(od => od.OrderId == orderId)
-                //                                                .Where(od => od.Status != 0)
-                //                                                .ToListAsync();
-                
-                return View(order);
+                var order = await _context.Orders.Include(o => o.User)
+                                                  .Include(od => od.OrderDetails)
+                                                  .ThenInclude(p => p.Product)
+                                                  .FirstOrDefaultAsync(o => o.Id == Id);
+                decimal total = 0;
+                foreach (var o in order.OrderDetails)
+                {
+                    total += o.Price * o.Quantity;
+                }
+
+                ViewBag.GrandTotal = total;
+	
+				return View(order);
             }
         }
-        public async Task<IActionResult> Delete(int Id)
+		public async Task<IActionResult> ViewOrderDetail(int Id)
+		{
+			{
+				List<OrderDetailModel> orderDetails = await _context.OrderDetails.Include(p => p.Product)
+                                                                                    .Include(od => od.Order)
+                                                                                    .ThenInclude(u  => u.User)
+                                                                                    .Where(od => od.OrderId == Id)
+                                                                                    .ToListAsync();
+
+				return View(orderDetails);
+			}
+		}
+
+        [HttpPost]
+        [Route("UpdateOrder")]
+        public async Task<IActionResult> UpdateOrder(int orderId, int status)
+		{
+			{
+                var order = await _context.Orders.FirstOrDefaultAsync(od => od.Id == orderId);
+
+				if(order == null){
+                    return NotFound();
+                }
+                order.Status = status;
+                try
+                {
+                    _context.Orders.Update(order);
+                    await _context.SaveChangesAsync();
+                    return Ok(new{success = true, message ="Update Order status successful"});
+
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(500, "Error");
+                }
+
+			}
+		}
+
+		public async Task<IActionResult> Delete(int Id)
         {
             {
                 OrderModel order = await _context.Orders.FindAsync(Id);
