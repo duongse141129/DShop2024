a56fbacbdd49679c3e4bd67602a4e2e6f2f42e78
duongse141129 <duong97498@gmail.com>
2024-12-25 23:53:03 +0700

    Setting FooterSlider

--- DShop2024/Controllers/HomeController.cs
+++ DShop2024/Controllers/HomeController.cs
@@ -44,10 +44,10 @@ namespace DShop2024.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        public IActionResult Contact()
+        public async Task<IActionResult> Contact()
         {
-
-            return View();
+            var contact = await _dataContext.Contacts.FirstOrDefaultAsync();
+            return View(contact);
         }
     }
 }
---
+++ DShop2024/Repository/Components/FooterViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DShop2024.Repository.Components
+{
+    public class FooterViewComponent : ViewComponent
+    {
+        private readonly DShopContext _dataContext;
+
+        public FooterViewComponent(DShopContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var contact = await _dataContext.Contacts               
+                                .FirstOrDefaultAsync();
+
+            return View(contact);
+
+        }
+    }
+}
