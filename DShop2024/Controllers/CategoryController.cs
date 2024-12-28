1c55c130c080cf1f6177947fad3d64fc470e52e6
duongse141129 <duong97498@gmail.com>
2024-12-29 00:00:14 +0700

    SortByPrice, Filter Price

--- DShop2024/Controllers/CategoryController.cs
+++ DShop2024/Controllers/CategoryController.cs
@@ -12,7 +12,7 @@ namespace DShop2024.Controllers
 		{
 			_dataContext = context;
 		}
-		public async Task<IActionResult> Index(string slug = "")
+		public async Task<IActionResult> Index(string slug = "", string sort_by = "", string startprice = "", string endprice = "")
 		{
 			CategoryModel category = await _dataContext.Categories
 										.Where(c => c.Status == 1)
@@ -23,8 +23,46 @@ namespace DShop2024.Controllers
 				return RedirectToAction("Index");
 			}
 
-			var productByCategory = await _dataContext.Products.Where(p => p.CategoryId == category.Id).ToListAsync();
-			productByCategory.OrderByDescending(c => c.Id);
+			IQueryable<ProductModel> productByCategory =  _dataContext.Products.Where(p => p.CategoryId == category.Id);
+			var count = await productByCategory.CountAsync();
+			if(count > 0)
+			{
+				if(sort_by == "price_increase")
+				{
+					productByCategory = productByCategory.OrderBy(p => p.Price);
+				}
+				else if (sort_by == "price_decrease")
+				{
+					productByCategory = productByCategory.OrderByDescending(p => p.Price);
+				}
+				else if (sort_by == "price_newest")
+				{
+					productByCategory = productByCategory.OrderByDescending(p => p.Id);
+				}
+				else if (sort_by == "price_oldest")
+				{
+					productByCategory = productByCategory.OrderBy(p => p.Id);
+				}
+				else if (startprice != "" && endprice != "")
+				{
+					decimal startPriceValue;
+					decimal endPriceValue;
+					if(decimal.TryParse(startprice, out startPriceValue) && decimal.TryParse(endprice,out endPriceValue))
+					{
+						productByCategory = productByCategory.Where(p => p.Price >= startPriceValue && p.Price <= endPriceValue);
+					}
+					else
+					{
+						productByCategory = productByCategory.OrderByDescending(p => p.Id);
+					}
+				}
+				else
+				{
+					productByCategory = productByCategory.OrderByDescending(p => p.Id);
+				}
+			}
+
+			await productByCategory.ToListAsync();
 			return View(productByCategory);
 
 		}
